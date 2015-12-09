cda5d01c7f2d56c018b6ada9cbbf32d856ea2052
Marko Lovic <markolovic@hotmail.com>
2015-12-09 11:45:29 +0100

    Add confirmation status to brands

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,6 +1,7 @@
 require 'active_record'
 
 class Brand < ActiveRecord::Base
+  enum confirmation_status: [ :unconfirmed, :confirmed ]
   #def destroy
     #raise ActiveRecord::ReadOnlyRecord
   #end
--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Brand do
     #brand = Brand.create(name: 'Mondraker')
     #expect { brand.destroy }.to raise_error ActiveRecord::ReadOnlyRecord
   #end
+  describe 'confirmation status' do
+    let!(:brand) { Brand.create name: 'Mondraker' }
+    it 'is unconfirmed by default' do
+      expect(brand.confirmed?).to be false
+      expect(brand.confirmation_status).to eq 'unconfirmed'
+    end
+
+    it 'can be confirmed' do
+      brand.confirmed!
+      expect(brand.confirmed?).to be true
+    end
+  end
 end
