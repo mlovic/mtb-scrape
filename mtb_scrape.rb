16820a2dcb6a44be79a365fda41375a50ca28fdc
Marko Lovic <markolovic@hotmail.com>
2016-01-04 18:55:27 +0100

    Move scrape operations out of cli to MtbScrape module

--- mtb_scrape.rb
+++ mtb_scrape.rb
@@ -25,11 +25,54 @@ module MtbScrape
     end
   end
 
+  def self.reset_bikes
+    # come up with method that tracks changes. At least number of records changed
+    Bike.delete_all
+    puts 'All bikes deleted'
+    Post.all.each do |post|
+      attributes = PostParser.parse(post)
+      next if attributes[:buyer]
+      bike = Bike.new(price: attributes[:price], 
+                      frame_only: attributes[:frame_only],
+                      size: attributes[:size],
+                      brand_id: attributes[:brand_id],
+                      model_id: attributes[:model_id],
+                      post_id: post.id
+                     )
+      bike.save!
+    end
+  end
+
+  def self.update(num_pages = 5)
+    new_posts = fmtb_scrape(num_pages) # 5? think about this
+    create_new_bikes(new_posts)
+  end
+
+  def self.create_new_bikes(posts)
+    posts.each do |post|
+      attributes = PostParser.parse(post)
+      next if attributes[:buyer]
+      bike = Bike.new(price: attributes[:price], 
+                      frame_only: attributes[:frame_only],
+                      size: attributes[:size],
+                      brand_id: attributes[:brand_id],
+                      model_id: attributes[:model_id],
+                      post_id: post.id
+                     )
+      bike.save!
+    end
+  end
+
+  def self.create_new_brand(name)
+    Brand.create(name: name, confirmation_status: 1)
+  end
+
   def self.fmtb_scrape(num_pages = 1, options = {})
 
     start_page = options[:start_page] || 1
     end_page = start_page + num_pages - 1
 
+    new_posts = []
 
     (start_page..end_page).each do |i|
 
@@ -52,18 +95,19 @@ module MtbScrape
         new_post = Post.new(p.scrape)
         if new_post.save
           puts "Post created successfully: #{new_post.title}" 
+          new_posts << new_post
         else
           p new_post
         end
       end
     end
 
-    puts "#{Post.all.size} posts in db"
+    puts "#{new_posts.size} new posts in db"
+    puts "#{Post.all.size} total posts in db"
+    return new_posts
   end
 
 
 
   ROOT_DIR = '/home/marko/mtb_scrape'
 end
-
-require 'pp'
--- mtb_scrape_cli.thor
+++ mtb_scrape_cli.thor
@@ -16,30 +16,21 @@ require 'mtb_scrape'
 
     desc 'parse_posts', 'parses all posts in db and saves them in bikes table'
     def parse_posts
-      Post.all.each do |post| 
-        next if Bike.exists?(post_id: post.id)
-        puts 'parsing  ' + post.title + '  - ' + post.id.to_s
-        attributes = PostParser.parse(post)
-        next if attributes[:buyer]
-        bike = Bike.new(price: attributes[:price], 
-                        frame_only: attributes[:frame_only],
-                        size: attributes[:size],
-                        brand_id: attributes[:brand_id],
-                        model_id: attributes[:model_id],
-                        post_id: post.id
-                       )
-        bike.save!
-      end
-      # posts.each
-      #   attrs = parse_post
-      #   bikes.new attrs if bikes doesnt exist
-      #
-      #   with logging
-      #
-      #
-      #
-      #
-      #
+    end
+
+    desc 'reset_bikes', 'deletes all bikes recreates them after parsing all posts'
+    def reset_bikes
+      MtbScrape::reset_bikes
+    end
+
+    desc 'update', 'scrapes foromtb and updates post information and bikes table'
+    def update
+      MtbScrape::update
+    end
+
+    desc 'new_brand NAME', 'ddssdfsdf'
+    def new_brand(name)
+      say 'Created new brand' if MtbScrape::create_new_brand(name)
     end
 
   end
