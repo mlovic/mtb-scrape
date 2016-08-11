2310973b0460de6e3b999d09fb3d6122179c8281
Marko Lovic <markolovic@hotmail.com>
2016-08-11 20:36:50 +0200

    Refactor Scraper to use async Spider and Handlers

--- lib/processor.rb
+++ lib/processor.rb
@@ -1,87 +1,25 @@
 class Processor
-  def initialize
+  def initialize(queue, *handlers)
+    @queue    = queue # does nothing with 2nd, alternative impl
+    @handlers = handlers
   end
 
-  # should extract processing into Processor class,
-  # which should be isolated from spider
-  # Keep this one to talk to spider and processor
-  # and handle concurrency if necessary
-
-  def process_list(page)
-    # posts = site.process_index
-    page.extend ListPage
-    page.posts.each { |post| eval_post(post) }
+  def match_handler(handler)
+    @handlers.find { |h| h.class == handler }
   end
 
-  def eval_post(post)
-    if new_post?(post) || title_changed?(post)
-      #spider.enqueue(post.url, :post)
-      store.enqueue_post(post.url, post.preview)
-    else 
-      update_last_msg(post)
+  def dispatch(page, handler)
+    target = match_handler(handler) or raise "Unknown handler! #{handler}"
+    target.process_page(page)
+  end
+
+  def start
+    until @queue.empty?
+      #if page_and_handler = @queue.pop
+        #page, handler = page_and_handler
+      page, handler = @queue.pop
+      target = match_handler(handler) or raise "Unknown handler! #{handler}"
+      target.process_page(page)
     end
   end
-
-  def title_changed?(post)
-    db_post = Post.find_by!(thread_id: post.thread_id)
-    post.title != db_post.title
-  end
-
-  def update_last_msg(post)
-    db_post = Post.find_by!(thread_id: post.thread_id)
-    unless db_post.last_message_at == post.last_message_at
-      puts 'updating last message...'
-      db_post.update last_message_at: post.last_message_at
-    end
-  end
-
-  def new_post?(post)
-    !Post.find_by(thread_id: post.thread_id)
-  end
-
-  ### POST
-  #
-  def process_post(page, url)
-    attrs = get_post_data(page, url)
-    process_post_data(attrs)
-  end
-  
-  def get_post_data(page, url)
-    # TODO page should carry url
-    page.extend PostPage
-    # attrs = site.process_page(page)
-    post_preview = store.get_prev(url)
-    attrs = page.all_attrs.merge post_preview.all_attrs
-  end
-
-  def process_post_data(attrs)
-    if db_post = Post.find_by(thread_id: attrs[:thread_id])
-      update_post(attrs, db_post)
-    else
-      create_post(attrs)
-    end
-  end
-
-  def update_post(attrs, db_post) 
-    # TODO keep title if vendida
-    #if db_post.title == attrs[title]
-    puts db_post.title
-    db_post.update(attrs)
-    puts "Post #{db_post.id} updated"
-    puts attrs[:title]
-  end
-
-  def create_post(attrs)
-    new_post = Post.create!(attrs)
-    puts "Post #{new_post.id} created"
-  end
-
-  def store
-    PostUriStore.instance
-  end
-
-  def spider
-    @spider ||= Spider.new(self)
-  end
-
 end
--- lib/spider.rb
+++ lib/spider.rb
@@ -1,63 +1,30 @@
-require 'benchmark'
-
 class Spider
-  # later try with enum instead of num urls
 
-  def initialize(processor, agent = Mechanize.new)
-    @processor = processor
+  def initialize(agent, time_between_requests: 2)
     @agent = agent
-    logger = Logger.new('mechanize.log')
-    logger.level = 'INFO'
-    agent.log = logger
-    @urls = {}
+    @waiting_for_response = false
+    @wait_time = time_between_requests
   end
-
-  def crawl
-    until store.empty?
-      url, type = store.take
-
-      page = nil
-      time = Benchmark.realtime do
-        page = @agent.get url
-      end
-
-      puts "retrieved #{type} page...  #{page.uri}  in #{time}s"
-      if type == :index
-        @processor.process_list(page)
-      elsif type == :post
-        @processor.process_post page, url
-      else
-        puts url
-        raise "Unknown type #{type}"
-      end
-    end
-      # abstract this to just some send_page method
+  
+  def crawl_async(in_queue, out_queue)
+    # TODO why mechanize? Switch to Net::HTTP?
+    # TODO error handling - check status, etc.
+      @thread = Thread.new do
+                  until in_queue.closed?
+                    # TODO no reason for spider to know about handler
+                    (url, handler = in_queue.pop) or Thread.exit
+                    @waiting_for_response = true
+                    puts "Fetching #{url}..."
+                    page = @agent.get(url)
+                    @waiting_for_response = false
+                    puts "Done fetching #{url}"
+                    out_queue.push [page, handler]
+                    sleep @wait_time # sleep variable amount of time depending on request time?
+                  end
+                end
   end
-
-  def enqueue_index(url)
-    store.enqueue_index(url)
+  
+  def waiting_for_response?
+    @waiting_for_response
   end
-
-  def enqueue(url, type)
-    #PostUriStore.set(post.url)
-    #@urls[url] = type
-    # TODO is this right?
-  end
-
-
-  # TODO get rid of this. Follow crawl + enqueue sys
-  #def scrape(url)
-    #puts 'visiting ' + url + ' ...' 
-    #post_page = @agent.get URI.join(ForoMtb::ROOT, url)
-    #post_page.extend PostPage
-    #return post_page
-  #end
-
-  private
-
-
-    def store
-      PostUriStore.instance
-    end
-
 end
