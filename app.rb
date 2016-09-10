f95585c31b50e949c22b289c668ea56d69142a9f
Marko Lovic <markolovic@hotmail.com>
2016-09-10 13:20:15 +0200

    Clean up app.rb code and comments

--- app.rb
+++ app.rb
@@ -1,19 +1,16 @@
 $:.unshift File.dirname(__FILE__)
 
-require 'sinatra'
 require 'pp'
+require 'sinatra'
 require "sinatra/activerecord"
 require 'will_paginate'
-require 'will_paginate/active_record'  # or data_mapper/sequel
+require 'will_paginate/active_record'
 require "will_paginate-bootstrap"
 
 require 'lib/bike'
 require 'lib/brand'
 require 'lib/model'
 require 'lib/post'
-require 'mtb_scrape'
-
-p ENV['RACK_ENV']
 
 configure :development do
   require 'thin'
@@ -34,31 +31,15 @@ configure :production do
   set :server, 'unicorn'
   set :database, {adapter: "sqlite3", database: "db/foromtb.db"}
 end
-#pp ENV
 
 helpers do
-  def word_after_brand(bike)
-    brand_name, str = bike.brand_name, bike.title # move outside of helper?
-    brand_name ? str.split(/#{brand_name}/i).last.split(' ').first.to_s : nil
-  end
-
   def humanize(secs)
     { d: 60*60*24, h: 60*60, m: 60, s: 1 }.each do |k, v|
       next if secs < v
       return "#{(secs / v).to_i} #{k}"
     end
   end
-
 end
-
-# actually fix this at some point
-before do
-  p params
-end
-
-after do
-  ActiveRecord::Base.clear_active_connections!
-end 
 
 get '/' do
   # TODO joins and pluck to reduce num of db calls
@@ -72,14 +53,8 @@ get '/' do
   erb :index
 end
 
-post 'bikes/:id/add-travel' do |id|
-  Bike.find(id).model.update!(travel: params['travel'])
-  # error unless if travel.valid?
-end
-
 post '/brands/:id/confirm' do |id|
   Brand.find(id).confirmed!
-  # take away and put into mtbscrape
 end
 
 delete '/brands/:id' do |id|
@@ -88,16 +63,29 @@ delete '/brands/:id' do |id|
   brand.destroy
 end
 
+get '/models' do
+  content_type :json
+  if params['brand_id']
+    Model.confirmed.where(brand_id: params['brand_id']).select(:id, :name).to_json
+  end
+end
+
 post '/update-model' do
   # TODO what if bike is not found. Other exception handling
   # or if invalid
   # or no change
   # or name has been taken
-  model = Bike.find(params['bike_id']).model
+  model = Bike.find!(params['bike_id']).model
   puts "Changing model name from #{model.name} to #{params['value']} and confirming"
-  model.update(name: params['value'])
+  model.update!(name: params['value'])
   model.confirmed! # maybe unwise
   params['value']
+end
+
+post '/models/:id/confirm' do |id|
+  model = Model.find(id)
+  model.confirmed!
+  model.brand.confirmed!
 end
 
 # Not used for now
@@ -108,34 +96,15 @@ post '/update-submodel' do
   params['value']
 end
 
-post '/brands/:id/confirm' do |id|
-  Brand.find(id).confirmed!
+post 'bikes/:id/add-travel' do |id|
+  Bike.find(id).model.update!(travel: params['travel']) 
+  # TODO show error when invalid
 end
 
-post '/models/:id/confirm' do |id|
-  #bike = Bike.find(params['bike_id'])
-  Model.find(id).confirmed!
-  # confirm bike as well?
-  #
-  #model = Model.create!(name: params['model_name'].titleize, 
-                        #brand_id: bike.brand.id)
-  #bike.model = model
-  # update other bikes with same model name
-  # i think we should follow same confirmation_status system as with brands
-  #bike.!save
-end
-
+# Not used for now
 get '/bikes/:thread_id' do |id|
-  p id.class
-  @post = Post.where(thread_id: id.to_i).take
+  @post = Post.find_by(thread_id: id.to_i)
   erb :show
-end
-
-get '/models' do
-  content_type :json
-  if params['brand_id']
-    Model.confirmed.where(brand_id: params['brand_id']).select(:id, :name).to_json
-  end
 end
 
 # Not used
@@ -149,5 +118,4 @@ get '/data' do
   puts 'parsed posts'
   #pp @posts
   @posts = @posts.compact.to_json.gsub('null', '""') # TODO fix hack, move to js?
-  @posts
 end
